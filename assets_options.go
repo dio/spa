b5c513a17a6ce68c50d799c88d4edfc070893f4c
Dhi Aurrahman <dio@rockybars.com>
2023-08-20 07:01:39 +0700

    Add index renderer
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- assets_options.go
+++ assets_options.go
@@ -14,16 +14,16 @@ func WithPrefix(pattern, prefix string) AssetsOption {
 		pattern = pattern + "/"
 	}
 	return func(a *Assets) error {
-		a.statics = make(map[string]string, len(a.statics))
-		fs.WalkDir(a.files, ".", func(entry string, d fs.DirEntry, err error) error {
+		a.Statics = make(map[string]AssetInfo, len(a.Statics))
+		fs.WalkDir(a.Files, ".", func(entry string, d fs.DirEntry, err error) error {
 			if d.IsDir() {
 				return nil
 			}
-			data, _ := fs.ReadFile(a.files, entry)
+			data, _ := fs.ReadFile(a.Files, entry)
 			content := string(data)
 			content = strings.ReplaceAll(content, pattern, prefix)
-			a.statics[path.Join("/", prefix, entry)] = entry
-			return a.files.WriteFile(entry, []byte(content), os.ModePerm)
+			a.Statics[path.Join("/", prefix, entry)] = AssetInfo{Path: entry}
+			return a.Files.WriteFile(entry, []byte(content), os.ModePerm)
 		})
 		return nil
 	}
